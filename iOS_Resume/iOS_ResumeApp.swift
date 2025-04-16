cc71801f88ede8706cefff4d37c6f06fc74c7b26
Purva <ptand@uic.edu>
2025-04-15 21:54:44 -0500

    Initial Commit

---
+++ iOS_Resume/iOS_ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ResumeApp.swift
+//  iOS_Resume
+//
+//  Created by Purva Tandel on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS_ResumeTests/iOS_ResumeTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ResumeTests.swift
+//  iOS_ResumeTests
+//
+//  Created by Purva Tandel on 4/15/25.
+//
+
+import Testing
+@testable import iOS_Resume
+
+struct iOS_ResumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
